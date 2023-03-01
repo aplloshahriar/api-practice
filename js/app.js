0a020f088e6103d4bff04a5e858f40385ece033f
aplloshahriar <aaronshahriar.as@gmail.com>
2023-03-01 17:48:26 +0600

    added play button

--- js/app.js
+++ js/app.js
@@ -5,14 +5,36 @@ const handleSearch=()=>{
     const inputValue= document.getElementById('text-value').value ;
     if(inputValue){
         fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
-.then(response => response.json())
-.then(data=>console.log(data))
+.then((response) => response.json())
+.then((data)=>{
+    if(data.title==="No Definitions Found"){
+        alert(data.message);
+    }
+    else{
+        displayResult(data);
+    }
+});
     
 }
 else{
-    alert('empty input')
+    alert('empty input');
 }
 };
-const displayResult=()=>{
-    const parent=document.getElementById('audio-container')
+const displayResult=(data)=>{
+    const parent=document.getElementById('audio-container');
+    data[0].phonetics.forEach((element)=>{
+        const Audio=document.createElement('audio');
+        Audio.src=element.audio;
+        // console.log(Audio);
+        
+        const button=document.createElement('button');
+        button.innerHTML="play";
+        button.onclick=()=>{
+            Audio.play();
+        };
+        const container=document.createElement('div');
+        container.appendChild(button);
+        container.appendChild(Audio);
+        parent.appendChild(container);
+    });
 };
